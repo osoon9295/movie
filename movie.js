f6e8e59082f8e9838fc5cb62cd96135b7b056491
Boyoung Lee <genuss040822@gamil.com>
2024-04-30 16:34:17 +0900

    검색기능 완료

--- movie.js
+++ movie.js
@@ -3,16 +3,13 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjhjY2Y1OTI5NTQ5OWYxYmJhODgzOTJlZWRjZWIyMCIsInN1YiI6IjY2MmEzMjc3ZGUxMWU1MDA5YjcwZTg4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OGPqGSssL5SW0ZxO3aZx2708eovOcaKRvbTOQQeFT8A",
-  },
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjhjY2Y1OTI5NTQ5OWYxYmJhODgzOTJlZWRjZWIyMCIsInN1YiI6IjY2MmEzMjc3ZGUxMWU1MDA5YjcwZTg4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OGPqGSssL5SW0ZxO3aZx2708eovOcaKRvbTOQQeFT8A"
+  }
 };
 
 const cardList = document.querySelector(".card-list");
 
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-)
+fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
   .then((response) => response.json())
   .then((response) => {
     let movies = response.results;
@@ -27,7 +24,7 @@ fetch(
 
       const id = a["id"]; // id
 
-      const card = document.createElement("div");
+      const card = document.createElement("li");
       card.classList.add("card");
       card.addEventListener("click", () => {
         alert(`영화 ID: ${id}`);
@@ -54,10 +51,42 @@ fetch(
 
     searchButton.addEventListener("click", () => {
       const inputText = searchInput.value.toLowerCase();
+      searchFunction(inputText); // 함수를 사용하여 결과를 필터링하고 표시
     });
 
     function searchFunction(val = "") {
-      movies = response.results.filter((value) => value.title.includes(val));
+      movies = response.results.filter((movie) => movie.title.toLowerCase().includes(val));
+      cardList.innerHTML = ""; // 기존 카드 지우기
+      movies.forEach((a) => {
+        // movies를 표시하기 위해 forEach 루프를 여기서 반복
+        const img = a["poster_path"]; // 포스터
+        const title = a["title"]; // 제목
+        const overview = a["overview"]; // 요약 내용
+        const voteAverage = a["vote_average"]; // 평점
+
+        const id = a["id"]; // id
+
+        const card = document.createElement("li");
+        card.classList.add("card");
+        card.addEventListener("click", () => {
+          alert(`영화 ID: ${id}`);
+        });
+
+        const cardImage = document.createElement("img");
+        cardImage.src = `https://image.tmdb.org/t/p/w200/${img}`;
+
+        const cardTitle = document.createElement("h1");
+        cardTitle.textContent = title;
+
+        const cardOverview = document.createElement("p");
+        cardOverview.textContent = overview;
+
+        const cardVoteAverage = document.createElement("p");
+        cardVoteAverage.textContent = voteAverage;
+
+        card.append(cardImage, cardTitle, cardOverview, cardVoteAverage);
+        cardList.append(card);
+      });
     }
   })
 
